b47c5b8056eae3ae158ba624716be2a98dcd059e
NoOneISeQual <kurdefrance1613@gmail.com>
2023-03-07 14:18:02 +0300

    Add

---
+++ Seminars/Work/Program.cs
@@ -0,0 +1,37 @@
+﻿// // Ввод чисел
+
+// Console.Clear(); // очистка консоли
+// // int n = Convert.ToInt32(Console.ReadLine());
+// int n = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(n * 3);
+
+//if; else
+
+// Console.Clear();
+// Console.Write("Введите 1-ое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 2-ое число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// // Интерполяция строк
+// // Console.WriteLine($"Результат {a} + {b} = {a + b}");
+// // Console.WriteLine("Результат " + a + " + " + b + " = " + (a + b));
+// // Console.WriteLine(a > b);
+// if (a > b)
+//     Console.WriteLine(a);
+// else if (a < b)
+//     Console.WriteLine(b);
+// else
+//     Console.WriteLine("=");
+
+//for(begin;end;step)
+//i++ <=> i= i + 1
+
+// for(int i = 1; i <= 10; i++) // 1 2 3 4 5 6 7 8 9 10 
+//     Console.Write($"{i} ");
+// Console.WriteLine();
+// for(int i = 1; i <= 10; i+=2) // 1 3 5 7 9
+//     Console.Write($"{i} ");
+// Console.WriteLine();
+// for(int i = 10; i > 0; i--) // 10 9 8 7 6 5 4 3 2 1
+//     Console.Write($"{i} ");
+
