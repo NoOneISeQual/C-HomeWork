20b74c0f310dea2e697bff52b51b2f7028ed5911
NoOneISeQual <kurdefrance1613@gmail.com>
2023-03-07 14:24:40 +0300

    Add code

--- Seminars/Work/Program.cs
+++ Seminars/Work/Program.cs
@@ -1,12 +1,19 @@
-﻿// // Ввод чисел
+﻿// Ввод чисел
 
 // Console.Clear(); // очистка консоли
-// // int n = Convert.ToInt32(Console.ReadLine());
+// int n = Convert.ToInt32(Console.ReadLine());
 // int n = int.Parse(Console.ReadLine()!);
 // Console.WriteLine(n * 3);
+// ctrl + / - комментирование и раскомментирование
+/*
+программный код
+Hello, world!
+*/
 
-//if; else
-
+// Необходимо спросить у пользователя 2 числа и узнать, какое из них наибольшее.
+// 1 > 2
+// 2 > 1
+// 1 = 2
 // Console.Clear();
 // Console.Write("Введите 1-ое число: ");
 // int a = Convert.ToInt32(Console.ReadLine());
@@ -16,16 +23,15 @@
 // // Console.WriteLine($"Результат {a} + {b} = {a + b}");
 // // Console.WriteLine("Результат " + a + " + " + b + " = " + (a + b));
 // // Console.WriteLine(a > b);
-// if (a > b)
+// if (a > b) 
 //     Console.WriteLine(a);
 // else if (a < b)
 //     Console.WriteLine(b);
 // else
 //     Console.WriteLine("=");
 
-//for(begin;end;step)
-//i++ <=> i= i + 1
-
+// for (begin; end; step)
+// i++ <=> i = i + 1
 // for(int i = 1; i <= 10; i++) // 1 2 3 4 5 6 7 8 9 10 
 //     Console.Write($"{i} ");
 // Console.WriteLine();
@@ -35,3 +41,15 @@
 // for(int i = 10; i > 0; i--) // 10 9 8 7 6 5 4 3 2 1
 //     Console.Write($"{i} ");
 
+// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное 
+// на само себя).
+
+// Например:
+// 4  -> 16 
+// -3 -> 9 
+// -7 -> 49
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.WriteLine(n * n);
