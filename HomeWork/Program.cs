bb0173eab1e0e28228a99ee65a54c5e275df37b0
NoOneISeQual <kurdefrance1613@gmail.com>
2023-03-10 14:19:37 +0300

    Task1

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,4 +1,4 @@
-﻿# Task 1
+﻿// # Task 1
 
 Console.Clear();
 Console.WriteLine("Введите 1 число: ");
@@ -6,6 +6,6 @@ int n = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите 2 число: ");
 int m = Convert.ToInt32(Console.ReadLine());
 if (n > m)
-  Console.WriteLine(n);
+  Console.WriteLine($"Наибольшее число"{n});
 else
-  Console.WriteLine(m);
+  Console.WriteLine($"Наибольшее число"{m});
