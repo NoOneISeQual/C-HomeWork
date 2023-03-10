6cb6a79f14d4bb3393a703a3f91534a7b95d8b36
NoOneISeQual <kurdefrance1613@gmail.com>
2023-03-10 14:45:51 +0300

    Add New Files

--- HomeWork/Task3/Program.cs
+++ HomeWork/Task3/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n / 2)
+  Console.WriteLine("Да");
+else
+  Console.WriteLine("Нет");
