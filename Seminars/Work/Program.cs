1916ee88c6ed97d18ccb3a040df4ea2fd366a2df
NoOneISeQual <kurdefrance1613@gmail.com>
2023-03-10 13:13:44 +0300

    Task 1

--- Seminars/Work/Program.cs
+++ Seminars/Work/Program.cs
@@ -156,3 +156,14 @@ Hello, world!
 // Console.Write("Введите число: ");
 // int n = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine(n % 10);
+
+Console.Clear();
+int n = New Random(). Next (10, 100);
+Console.WriteLine($"Случайное сгенерированное число {n}");
+int n1 = n / 10;
+int n2 = n % 10;
+if (n1 > n2);
+  Console.WriteLine(n1);
+else
+  Console.WriteLine(n2);
+  
