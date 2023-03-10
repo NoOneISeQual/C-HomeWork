d933e69239569cf447c2fd4d8ba959cba083bc98
NoOneISeQual <kurdefrance1613@gmail.com>
2023-03-10 13:16:50 +0300

    Task 1 Fix

--- Seminars/Work/Program.cs
+++ Seminars/Work/Program.cs
@@ -162,7 +162,7 @@ int n = new Random().Next(10, 100);
 Console.WriteLine($"Случайное сгенерированное число {n}");
 int n1 = n / 10;
 int n2 = n % 10;
-if (n1 > n2);
+if (n1 > n2)
   Console.WriteLine(n1);
 else
   Console.WriteLine(n2);
