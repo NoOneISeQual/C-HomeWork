6433d75b4282186118229a687fda636cf3d552af
NoOneISeQual <kurdefrance1613@gmail.com>
2023-03-10 14:23:55 +0300

    Task 1

--- HomeWork/Task1/Program.cs
+++ HomeWork/Task1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите 1 число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n > m)
+  Console.WriteLine(n);
+else
+  Console.WriteLine(m);
