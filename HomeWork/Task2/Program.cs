95e5aba9fa631911e3c7cb9379e57d40889f5214
NoOneISeQual <kurdefrance1613@gmail.com>
2023-03-10 14:38:46 +0300

    Add Task2

--- HomeWork/Task2/Program.cs
+++ HomeWork/Task2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите 1е число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2е число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3е число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+if (n > n1 && n > n2)
+  Console.WriteLine(n);
+else if (n1 > n && n1 > n2)
+  Console.WriteLine(n1);
+else if (n2 > n && n2 > n1)
+  Console.WriteLine(n2);
