3a5bdb57bc122f3aa1be40c5ab898e3f78547638
NoOneISeQual <kurdefrance1613@gmail.com>
2023-03-12 13:28:25 +0300

    Add Task 2_1

--- HomeWork/Task2_1/Program.cs
+++ HomeWork/Task2_1/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(n % 10);
