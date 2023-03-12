22568b1516434d35549cb47221fbe17c8941ad93
NoOneISeQual <kurdefrance1613@gmail.com>
2023-03-12 14:33:11 +0300

    Add Task 2_3

---
+++ HomeWork/Task2_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите день недели(от 1 до 7): ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 1 || n > 7)
+{
+  Console.WriteLine("Вы ошиблись! повторите попытку");
+}
+else if (n == 6 || n == 7)
+{
+  Console.WriteLine("Выходной");
+}
+else
+  Console.WriteLine("Будний день");
