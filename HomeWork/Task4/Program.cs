08db28e1cd54a83347b043b4b5737771f0f96a40
NoOneISeQual <kurdefrance1613@gmail.com>
2023-03-12 14:21:14 +0300

    переименовали

--- HomeWork/Task4/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// Console.Clear();
-// int N = new Random().Next(1,100);
-
-
-Console.Clear();
-Console.Write("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-for(int i = 2; i<= n; i = i + 2)
-  Console.WriteLine(i);
