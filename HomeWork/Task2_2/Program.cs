a7b0dc7801972aa335a809360f878029b0397e8f
NoOneISeQual <kurdefrance1613@gmail.com>
2023-03-12 14:19:56 +0300

    Add Task 2_2

--- HomeWork/Task2_2/Program.cs
+++ HomeWork/Task2_2/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-if (n > 999)
+while (n > 999)
 {
   n /= 10;
 }
-else if (n < 100)
+if (n < 100)
 {
   Console.WriteLine("Нет циферки");
 }
