698924134bd8dd4c654a3df6a2963ca0e820eea0
NoOneISeQual <kurdefrance1613@gmail.com>
2023-03-19 18:25:21 +0300

    Add Task 1

--- HomeWork/Home3/Task 1/Program.cs
+++ HomeWork/Home3/Task 1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Clear();
+Console.WriteLine("Введите число: ");
+string a = Console.ReadLine()!;
+int n = a.Length;
+if (n == 5)
+{
+    if (a[0] == a[4] && a[1] == a[3])
+    {
+        Console.WriteLine($"{a} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{a} - Не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Не является пятизначным числом");
+}
