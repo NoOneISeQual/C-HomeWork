5f3260dc6c4d5495cea591f0ac44986e927666f6
NoOneISeQual <kurdefrance1613@gmail.com>
2023-03-19 17:36:56 +0300

    переименовали в задание 3

--- HomeWork/Home3/Task 1/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// принимает число (N) и выдает таблицу кубов от 1 до N
-Console.Clear();
-Console.Write("Введите число: ");
-int n = int.Parse(Console.ReadLine()!);
-for (int i = 1; i <= n; i++)
-    Console.WriteLine($"{i} ^ 3 = {i * i}");
--- HomeWork/Home3/Task 2/Program.cs
+++ HomeWork/Home3/Task 2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Принимает координаты двух точек инаходит расстояние между точками в 2D простран стве
+Console.Clear();
+Console.Write("Введите координату X(1): ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y(1): ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату X(2): ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату Y(2): ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+double s = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine($"Расстояние между двумя точками равно {Math.Round(s, 2)}");
