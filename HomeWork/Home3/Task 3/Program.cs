fbe94948d6b9727c278c92111a03e014b7101903
NoOneISeQual <kurdefrance1613@gmail.com>
2023-03-19 17:37:51 +0300

    Task 3

---
+++ HomeWork/Home3/Task 3/Program.cs
@@ -0,0 +1,6 @@
+﻿// принимает число (N) и выдает таблицу кубов от 1 до N
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+for (int i = 1; i <= n; i++)
+    Console.WriteLine($"{i} ^ 3 = {i * i}");
