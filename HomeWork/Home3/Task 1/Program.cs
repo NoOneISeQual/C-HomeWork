9f66ccb2d019adf23614a12bee67ca6171c65f3a
NoOneISeQual <kurdefrance1613@gmail.com>
2023-03-19 17:24:07 +0300

    Add Task 1

--- HomeWork/Home3/Task 1/Program.cs
+++ HomeWork/Home3/Task 1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// принимает число (N) и выдает таблицу кубов от 1 до N
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+for (int i = 1; i <= n; i++)
+    Console.WriteLine($"{i} ^ 3 = {i * i}");
