fd9e4a4f4384f1c893a6f6f73a58ddf6b46e8253
NoOneISeQual <kurdefrance1613@gmail.com>
2023-03-21 15:48:08 +0300

    1 step

---
+++ HomeWork/Home4/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число А: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int m = int.Parse(Console.ReadLine()!);
+for (int i = 0; i <= m; i++ )
+{
+    n*=n;
+}
+Console.WriteLine(n);
