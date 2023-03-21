dbfeb8af97393f7e990d7977e9bc409196ab11f7
NoOneISeQual <kurdefrance1613@gmail.com>
2023-03-21 16:28:34 +0300

    Add Task 2

--- HomeWork/Home4/Task2/Program.cs
+++ HomeWork/Home4/Task2/Program.cs
@@ -1,3 +1,7 @@
 ﻿Console.Clear();
-Console.Write("Введите число :");
-int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число: ");
+string n = Console.ReadLine()!;
+int sum = 0;
+for (int i = 0; i < n.Length; i++)
+    sum = sum + int.Parse(n[i].ToString());
+Console.WriteLine(sum);
